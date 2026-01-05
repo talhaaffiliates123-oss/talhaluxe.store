4c00ecab14bc354b6f9542fbac5fd24066c8f67d
Talha <talhaaffiliates123@gmail.com>
2026-01-05 16:12:10 +0000

    https://ibb.co/tPCYT3mn https://ibb.co/yFqbCZgr https://ibb.co/8g3DcgRC

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   env: {
