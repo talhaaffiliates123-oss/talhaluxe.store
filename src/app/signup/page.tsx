56b217aecd87595b7d81c107bcf51857ef12d963
Talha <talhaaffiliates123@gmail.com>
2026-01-06 17:13:39 +0000

    i have gave website link to my friend but when he is trying to creating

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -120,6 +120,7 @@ export default function SignupPage() {
                 type="email"
                 placeholder="you@example.com"
                 required
+                autoComplete="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
