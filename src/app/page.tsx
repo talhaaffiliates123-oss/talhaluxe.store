5ef027b2bd0b8090e4f3a38bc7287057a81f558a
Talha <talhaaffiliates123@gmail.com>
2026-01-14 17:43:58 +0000

    Expected first argument to collection() to be a CollectionReference, a D

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import { getProductsForServer } from '@/lib/products.server';
-import { getFirebaseAdmin } from '@/firebase/server-initialization';
+'use client';
+import { getProducts } from '@/lib/products';
+import { useFirestore } from '@/firebase';
+import { useEffect, useState } from 'react';
 import HomePageContent from '@/components/layout/home-page-content';
+import type { Product } from '@/lib/types';
 
-export default async function Home() {
-  const { firestore } = getFirebaseAdmin();
-  const products = await getProductsForServer(firestore);
+export default function Home() {
+  const firestore = useFirestore();
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (firestore) {
+      getProducts(firestore).then(setProducts);
+    }
+  }, [firestore]);
 
   return <HomePageContent products={products} />;
 }
