9712e836cc014e10f137c4bf142e49d44e80bd6d
Talha <talhaaffiliates123@gmail.com>
2026-01-18 17:47:55 +0000

    Failed to compile. 22:46:34 22:46:34 ./src/firebase/server-initializat

--- src/firebase/server-initialization.ts
+++ src/firebase/server-initialization.ts
@@ -1,18 +1,10 @@
 
-import * as admin from 'firebase-admin';
+// import * as admin from 'firebase-admin';
 
 // This function should only be called on the server.
 export function initializeFirebase() {
   if (typeof window !== 'undefined') {
     throw new Error('This function should only be called on the server.');
-  }
-
-  // Check if the app is already initialized to prevent errors
-  if (admin.apps.length > 0) {
-    return { 
-        app: admin.app(), 
-        firestore: admin.firestore() 
-    };
   }
 
   // This functionality is disabled because environment keys were removed.
